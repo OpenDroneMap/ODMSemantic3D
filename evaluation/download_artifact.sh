aadfe5476449a1da3ec23f5a659e82f0f20e1987
Luca Di Leo <admin@lucadileo.it>
2023-04-28 12:58:40 +0200

    Modified download artifact script

--- evaluation/download_artifact.sh
+++ evaluation/download_artifact.sh
@@ -1,26 +1,20 @@
 #!/bin/bash
 
 # Ensure that all required arguments are provided
-if [ $# -ne 5 ]; then
+if [ $# -ne 4 ]; then
   echo "Usage: download_artifact.sh <repo-owner> <repo-name> <workflow-name> <artifact-name> <token>"
   exit 1
 fi
 
 # Assign the provided arguments to bash variables
-REPO_OWNER=$1
-REPO_NAME=$2
-WORKFLOW_NAME=$3
-ARTIFACT_NAME=$4
-TOKEN=$5
+REPO=$1
+WORKFLOW_NAME=$2
+ARTIFACT_NAME=$3
+TOKEN=$4
 
 # Ensure that all the provided arguments are non-empty
-if [ -z "$REPO_OWNER" ]; then
+if [ -z "$REPO" ]; then
   echo "Repo owner is empty"
-  exit 1
-fi
-
-if [ -z "$REPO_NAME" ]; then
-  echo "Repo name is empty"
   exit 1
 fi
 
@@ -55,11 +49,11 @@ if ! command -v unzip &> /dev/null; then
   exit 1
 fi
 
-echo "Downloading artifact \"$ARTIFACT_NAME\" from workflow \"$WORKFLOW_NAME\" of \"$REPO_OWNER/$REPO_NAME\""
+echo "Downloading artifact \"$ARTIFACT_NAME\" from workflow \"$WORKFLOW_NAME\" of \"$REPO\""
 
 # Get the latest workflow run ID for the specific workflow
 RUN_ID=$(curl --silent --request GET \
-  --url "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/actions/workflows/$WORKFLOW_NAME/runs" \
+  --url "https://api.github.com/repos/$REPO/actions/workflows/$WORKFLOW_NAME/runs" \
   --header "Accept: application/vnd.github+json" \
   --header "X-GitHub-Api-Version: 2022-11-28" \
   --header "Authorization: Bearer $TOKEN" \
@@ -69,7 +63,7 @@ echo "Run ID: $RUN_ID"
 
 # Get the artifact download URL for the specific artifact
 ARTIFACT_DOWNLOAD_URL=$(curl --silent --request GET \
-  --url "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/actions/runs/$RUN_ID/artifacts" \
+  --url "https://api.github.com/repos/$REPO/actions/runs/$RUN_ID/artifacts" \
   --header "Accept: application/vnd.github+json" \
   --header "X-GitHub-Api-Version: 2022-11-28" \
   --header "Authorization: Bearer $TOKEN" \
