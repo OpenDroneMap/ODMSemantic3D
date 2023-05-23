07fdf82f9ed4b4736f5bc8f10db5d358d6536454
Luca Di Leo <admin@lucadileo.it>
2023-05-23 19:30:22 +0200

    Added training set field

--- evaluation/run.sh
+++ evaluation/run.sh
@@ -6,10 +6,18 @@ wget $latest_release_url -O opc.tar.gz
 tar -xzvf opc.tar.gz
 chmod +x pctrain pcclassify
 
-# Get the list of all point cloud file paths in the datasets repository excluding the ground-truth folder
-DATASET_FILES=$(find . -type f -iname "*.laz" -o -iname "*.las" -o -iname "*.ply" | grep -v "ground-truth")
+# settings.json has this format:
+# {
+#   "scales": 4,
+#   "resolution": 0.1,
+#   "radius": 0.5,
+#   "training_set": [
+#     "datasets/1.laz",
+#     "datasets/2.laz",
+#     "datasets/3.laz"
+#   ]
+# }
 
-echo "Dataset Files: $DATASET_FILES"
 
 # Load training settings from settings.json
 SCALES=$(jq '.scales' evaluation/settings.json)
@@ -20,7 +28,21 @@ echo "SCALES: $SCALES"
 echo "RESOLUTION: $RESOLUTION"
 echo "RADIUS: $RADIUS"
 
-# Execute pctrain on all point clouds from step 2 with the settings from step 3
+# Get the list of all added or edited point cloud file paths in the datasets repository excluding the ground-truth folder
+DATASET_FILES=$(git diff --name-only --diff-filter=AMR ${{ github.event.pull_request.base.sha }}..${{ github.event.pull_request.head.sha }} -- '*.laz' '*.las' '*.ply' | grep -v "ground-truth")
+echo "New or edited files: $DATASET_FILES"
+
+# Get the training_set array from settings.json and merge with the list of all added or edited point cloud file paths
+TRAINING_SET=$(jq '.training_set' evaluation/settings.json)
+
+echo "Training set: $TRAINING_SET"
+
+# Merge the two lists
+DATASET_FILES="$DATASET_FILES $TRAINING_SET"
+
+echo "New training set: $DATASET_FILES"
+
+# Execute pctrain on all point clouds with the provided settings
 ./pctrain --classifier gbt --scales $SCALES --resolution $RESOLUTION --radius $RADIUS --output model.bin $DATASET_FILES
 
 # Get the list of all point cloud file paths in the ground truth repository
@@ -30,7 +52,7 @@ echo "Ground Truth Files: $GROUND_TRUTH_FILES"
 
 NEW_STATS_FILES=""
 
-# Execute pcclassify for each point cloud from step 6 with the model.bin from step 4 and stats-file new-stats.json
+# Execute pcclassify for each point cloud in the ground truth repository
 for FILE in $GROUND_TRUTH_FILES; do
 
   OUTPUT_FILE="${FILE%.*}_classified.${FILE##*.}"
