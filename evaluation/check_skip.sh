7e6fa4b525cf9b04d5e3281e80987a1dd13a9bc0
Luca Di Leo <admin@lucadileo.it>
2023-05-24 18:37:53 +0200

    Using json field instead of github groups

--- evaluation/check_skip.sh
+++ evaluation/check_skip.sh
@@ -7,41 +7,25 @@ TOKEN=$4
 
 COMMENTERS_URL="https://api.github.com/repos/$REPO_FULLNAME/issues/$ISSUE_NUMBER/comments"
 
-echo "Commenters URL: $COMMENTERS_URL"
-
 # Get the commenters who have written '!skip'
 commenters_res=$(curl -s -H "Authorization: Bearer $TOKEN" \
                      -H "X-GitHub-Api-Version: 2022-11-28" \
                      -H "Accept: application/vnd.github.v3+json" \
                   $COMMENTERS_URL)
-
-echo "Commenters res: $commenters_res"
-
 commenters=$(echo $commenters_res | jq -r '.[] | select(.body == "!skip") | .user.login')
-
-echo "Commenters: $commenters"
 
 ORG_NAME=$(echo $REPO_FULLNAME | cut -f1 -d"/")
 
 echo "Org name: $ORG_NAME"
 
-TEAM_MEMBERS_URL="https://api.github.com/orgs/$ORG_NAME/teams/$TEAM_SLUG/members"
+# Get the field org_users from the evaluation/settings.json file
+org_users=$(jq '.org_users' evaluation/settings.json)
 
-echo "Team members URL: $TEAM_MEMBERS_URL"
-
-# Get the list of maintainers and developers
-team_members_res=$(curl -s -H "Authorization: Bearer $TOKEN" \
-                       -H "X-GitHub-Api-Version: 2022-11-28" \
-                       -H "Accept: application/vnd.github.v3+json" \
-                    $TEAM_MEMBERS_URL)
-
-echo "Team members res: $team_members_res"
-
-team_members=$(echo $team_members_res | jq -r '.[].login')
+echo "Org users: $org_users"
 
 # Check if any of the commenters who wrote '!skip' are in the maintainers/developers list
 for commenter in $commenters; do
-  if echo "$team_members" | grep -q "$commenter"; then
+  if echo "$org_users" | grep -q "$commenter"; then
 
     echo "Found !skip comment by $commenter"
 
