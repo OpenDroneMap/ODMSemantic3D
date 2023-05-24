664cef6668381262d263a5f62896004dcd80da91
Luca Di Leo <admin@lucadileo.it>
2023-05-24 17:22:51 +0200

    Improved CI

--- evaluation/check_skip.sh
+++ evaluation/check_skip.sh
@@ -3,14 +3,27 @@
 ISSUE_NUMBER=$1
 REPO_FULLNAME=$2
 TEAM_SLUG=$3
+TOKEN=$4
 
 # Get the commenters who have written '!skip'
-commenters=$(gh api repos/$REPO_FULLNAME/issues/$ISSUE_NUMBER/comments --paginate -q '.[] | select(.body == \"!skip\") | .user.login')
+commenters=$(curl -s -H "Authorization: Bearer $TOKEN" \
+                     -H "X-GitHub-Api-Version: 2022-11-28" \
+                     -H "Accept: application/vnd.github.v3+json" \
+                https://api.github.com/repos/$REPO_FULLNAME/issues/$ISSUE_NUMBER/comments \
+            | jq -r '.[] | select(.body == "!skip") | .user.login')
 
 echo "Commenters: $commenters"
 
+ORG_NAME=$(echo $REPO_FULLNAME | cut -f1 -d"/")
+
+echo "Org name: $ORG_NAME"
+
 # Get the list of maintainers and developers
-team_members=$(gh api orgs/$(echo $REPO_FULLNAME | cut -f1 -d"/")/teams/$TEAM_SLUG/members --paginate -q '.[].login')
+team_members=$(curl -s -H "Authorization: Bearer $TOKEN" \
+                       -H "X-GitHub-Api-Version: 2022-11-28" \
+                       -H "Accept: application/vnd.github.v3+json" \
+                    https://api.github.com/orgs/$ORG_NAME/teams/$TEAM_SLUG/members \
+              | jq -r '.[].login')
 
 # Check if any of the commenters who wrote '!skip' are in the maintainers/developers list
 for commenter in $commenters; do
