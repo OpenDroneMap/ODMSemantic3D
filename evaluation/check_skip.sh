8dab02be58b232d0a3d2918927c8ecd899319cb6
Luca Di Leo <admin@lucadileo.it>
2023-05-24 18:11:37 +0200

    Fixed CI

--- evaluation/check_skip.sh
+++ evaluation/check_skip.sh
@@ -5,12 +5,17 @@ REPO_FULLNAME=$2
 TEAM_SLUG=$3
 TOKEN=$4
 
+COMMENTERS_URL="https://api.github.com/repos/$REPO_FULLNAME/issues/$ISSUE_NUMBER/comments"
+
 # Get the commenters who have written '!skip'
-commenters=$(curl -s -H "Authorization: Bearer $TOKEN" \
+commenters_res=$(curl -s -H "Authorization: Bearer $TOKEN" \
                      -H "X-GitHub-Api-Version: 2022-11-28" \
                      -H "Accept: application/vnd.github.v3+json" \
-                https://api.github.com/repos/$REPO_FULLNAME/issues/$ISSUE_NUMBER/comments \
-            | jq -r '.[] | select(.body == "!skip") | .user.login')
+                  $COMMENTERS_URL)
+
+echo "Commenters res: $commenters_res"
+
+commenters=$(echo $commenters_res | jq -r '.[] | select(.body == "!skip") | .user.login')
 
 echo "Commenters: $commenters"
 
@@ -18,12 +23,17 @@ ORG_NAME=$(echo $REPO_FULLNAME | cut -f1 -d"/")
 
 echo "Org name: $ORG_NAME"
 
+TEAM_MEMBERS_URL="https://api.github.com/orgs/$ORG_NAME/teams/$TEAM_SLUG/members"
+
 # Get the list of maintainers and developers
-team_members=$(curl -s -H "Authorization: Bearer $TOKEN" \
+team_members_res=$(curl -s -H "Authorization: Bearer $TOKEN" \
                        -H "X-GitHub-Api-Version: 2022-11-28" \
                        -H "Accept: application/vnd.github.v3+json" \
-                    https://api.github.com/orgs/$ORG_NAME/teams/$TEAM_SLUG/members \
-              | jq -r '.[].login')
+                    $TEAM_MEMBERS_URL)
+
+echo "Team members res: $team_members_res"
+
+team_members=$(echo $team_members_res | jq -r '.[].login')
 
 # Check if any of the commenters who wrote '!skip' are in the maintainers/developers list
 for commenter in $commenters; do
@@ -36,5 +46,6 @@ for commenter in $commenters; do
   fi
 done
 
+echo "No !skip comment found"
 # No skip command found, exit normally
 exit 0
